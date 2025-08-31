9d987d86f0f19422d121c47934ecd38d189f6d3b
Jahnavi Devarinti <devarinti.jahnu@gmail.com>
2025-08-31 12:43:35 +0530

    Add files via upload
    Prints the array elements which were accessed through index

---
+++ ArrayInput.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class ArrayInput
+{
+    public static void main(String[] args)
+    {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the size of an Array:");
+        int size=sc.nextInt();
+        int [] arr = new int[size];
+        System.out.println("Enter "+size+" Numbers");
+        for(int i=0;i<size;i++)
+         arr[i] =sc.nextInt();
+        System.out.println("Array elements are: ");
+        for(int i=0;i<size;i++)
+         System.out.println(arr[i]+" ");
+    }
+}
