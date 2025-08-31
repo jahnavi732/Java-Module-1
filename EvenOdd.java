6c6e96d26df50d5bffecd38eaaa9353ecd89717a
Jahnavi Devarinti <devarinti.jahnu@gmail.com>
2025-08-31 12:40:36 +0530

    Add files via upload
    Checks whether the entered number is even or odd

---
+++ EvenOdd.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+class EvenOdd{
+    public static void main(String[] args)
+    {
+        System.out.println("Enter a number:");
+        Scanner sc = new Scanner(System.in);
+        int a=sc.nextInt();
+        if(a%2==0)
+            System.out.println("The entered number is Even");
+        else
+            System.out.println("The entered number is Odd");
+        
+    }
+}
