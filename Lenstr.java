be6c1e7e7792aca9d1b3988526ac4f87a8455a8a
Jahnavi Devarinti <devarinti.jahnu@gmail.com>
2025-09-02 21:10:42 +0530

    Add files via upload

---
+++ Lenstr.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Lenstr
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Enter a string: ");
+        Scanner sc=new Scanner(System.in);
+        String s1=sc.nextLine();
+        System.out.println("Length of the string is: "+s1.length());
+    }
+}
